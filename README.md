2e9c8c67e2b4b4f45840959f7e1031ba3ef9b543
Megan Coyle <megan@mcoyle.com>
2020-09-12 19:07:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gatsby Blog
+
+Test project to study how to work with Gatsby.
+
+## To Run Locally
+
+Run `npm install`, then `npm start`
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+export default () => (
+  <Layout>
+    <h1>About Me</h1>
+    <p>This is my website.</p>
+    <Link to="/">Home</Link>
+  </Layout>
+);
